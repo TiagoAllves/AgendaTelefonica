422658c35a3c9850c33b4bb13fdc6cd9472c35be
ThiagoNOliveira <eno.thiago@gmail.com>
2013-09-04 16:55:19 -0300

    Mapeamento de pessoa e endereço

---
+++ AgendaTelefonica.Domain/Mapeamento/EnderecoMap.cs
@@ -0,0 +1,19 @@
+﻿using AgendaTelefonica.Domain.Entidades;
+using FluentNHibernate.Mapping;
+
+namespace AgendaTelefonica.Domain.Mapeamento
+{
+    public class EnderecoMap : ClassMap<Endereco>
+    {
+        EnderecoMap()
+        {
+            Id(e => e.Id);
+            Map(e => e.Rua);
+            Map(e => e.Numero);
+            Map(e => e.Bairro);
+            Map(e => e.Cidade);
+            Map(e => e.Estado);
+            Map(e => e.Pais);
+        }
+    }
+}
---
+++ AgendaTelefonica.Domain/Mapeamento/PessoaMap.cs
@@ -0,0 +1,17 @@
+﻿using AgendaTelefonica.Domain.Entidades;
+using FluentNHibernate.Mapping;
+
+namespace AgendaTelefonica.Domain.Mapeamento
+{
+    public class PessoaMap : ClassMap<Pessoa>
+    {
+        PessoaMap()
+        {
+            Id(p => p.Id);
+            Map(p => p.Nome);
+            Map(p => p.Idade);
+            Map(p => p.Telefone);
+            Map(p => p.Cpf);
+        }
+    }
+}
